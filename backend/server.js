8971caa480277c77b63e9ec640c2fd1a6373f728
Rafal Zakrzewski <86196784+Garusxx@users.noreply.github.com>
2024-06-05 15:30:39 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Helssslo World!");
+  res.send("Yesss World!");
 });
 
 app.get("/getHousing", async (req, res) => {
